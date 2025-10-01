4d436def8ac5d58a3797a03ff9677f1daec33b4a
fmgarcia18 <fmgarcia18@gmail.com>
2025-10-01 09:55:13 -0300

    Primer commit subiendo estructura de projecto

---
+++ src/main/java/com/fmgarcia/service/management/FmgarciaServiceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.fmgarcia.service.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmgarciaServiceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmgarciaServiceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmgarcia/service/management/FmgarciaServiceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmgarcia.service.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmgarciaServiceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
